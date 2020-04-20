0476c605c36114a6f994ebf2dc00aca304053a6e
Keval Solanki <57882119+keval8solanki@users.noreply.github.com>
2020-04-20 17:58:50 +0530

    Updates

---
+++ precache-manifest.b277d366aff55c335663ade35be8054a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859ec543d2efc29da00f12d4e93fd6a9",
+    "url": "/status-client/index.html"
+  },
+  {
+    "revision": "64bfa3157d2f0dd29007",
+    "url": "/status-client/static/css/main.83f2456d.chunk.css"
+  },
+  {
+    "revision": "a9e373b20dfbf5ba1cf3",
+    "url": "/status-client/static/js/2.86dd779f.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/status-client/static/js/2.86dd779f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64bfa3157d2f0dd29007",
+    "url": "/status-client/static/js/main.072dc144.chunk.js"
+  },
+  {
+    "revision": "e1941912602c3f7b7492",
+    "url": "/status-client/static/js/runtime-main.556308e7.js"
+  }
+]);
